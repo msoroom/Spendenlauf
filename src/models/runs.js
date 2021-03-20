c4ab81b0d21b1b4bb2b6241516bcee657502ee6a
21Jon <l.jonas.1@web.de>
2021-03-20 21:50:09 +0100

    ups

--- src/models/runs.js
+++ src/models/runs.js
@@ -6,16 +6,16 @@ const runSchema = new mongoose.Schema({
     distance:{
         type: Number,
         require : true,
-        // min: 1,
-        // max:20,
-        // validate(value) {
+        min: 1,
+        max:20,
+        validate(value) {
 
-        //     if(value == null){
+            if(value == null){
 
-        //         throw new Error('Value cannot be null')
+                throw new Error('Value cannot be null')
 
-        //     }
-        // }
+            }
+        }
     }, 
 
     owner:{
